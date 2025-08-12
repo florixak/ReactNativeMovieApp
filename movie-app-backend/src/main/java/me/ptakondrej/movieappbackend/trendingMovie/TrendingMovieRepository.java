91ba53ef3759a6df7c27ab5beafba2dd19c486da
ptakondrej <florixakcz@seznam.cz>
2025-08-12 16:54:06 +0200

    Add Repository annotation

--- movie-app-backend/src/main/java/me/ptakondrej/movieappbackend/trendingMovie/TrendingMovieRepository.java
+++ movie-app-backend/src/main/java/me/ptakondrej/movieappbackend/trendingMovie/TrendingMovieRepository.java
@@ -1,10 +1,12 @@
 package me.ptakondrej.movieappbackend.trendingMovie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TrendingMovieRepository extends JpaRepository<TrendingMovie, Long> {
 	List<TrendingMovie> findTop10ByOrderByCountDesc();
 	Optional<TrendingMovie> findByMovieIdAndSearchTerm(Long movieId, String searchTerm);

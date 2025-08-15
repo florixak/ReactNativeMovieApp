3b0aaee4cd77d1db0bd8fc8015f3d92a3ceb1e3e
ptakondrej <florixakcz@seznam.cz>
2025-08-15 13:12:10 +0200

    Add React Query integration to app layout
    Integrated @tanstack/react-query by wrapping the app's root layout with QueryClientProvider. Updated dependencies in package.json to include @tanstack/react-query for state and server data management.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
 import { AuthProvider } from "@/contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 import "./globals.css";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <StatusBar hidden />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <StatusBar hidden />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }

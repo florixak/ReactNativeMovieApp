849cba71853afd13332a606a990354394989e36b
ptakondrej <florixakcz@seznam.cz>
2025-08-16 10:02:36 +0200

    Move storage constants to their file

---
+++ constants/storage.ts
@@ -0,0 +1,4 @@
+export const TOKEN_KEY = "auth_token";
+export const REFRESH_TOKEN_KEY = "auth_refresh_token";
+export const USER_KEY = "auth_user";
+export const EXPIRES_IN_KEY = "auth_expires_in";
